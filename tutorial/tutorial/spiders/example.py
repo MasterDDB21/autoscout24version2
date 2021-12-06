99e2c27cfda08b5941ef697fbcf559b6c108ea2d
Rob Loke <44401840+lokerob@users.noreply.github.com>
2021-12-06 11:39:58 +0100

    attempt 2
    1st attempt to crawl to next page but did not work probably because of javascript interaction is needed

--- tutorial/tutorial/spiders/example.py
+++ tutorial/tutorial/spiders/example.py
@@ -5,7 +5,9 @@ class ExampleSpider(scrapy.Spider):
     name = 'example'
     allowed_domains = ['autoscout24.com']
     #   start_urls = [r"C:\Users\roblo\autoscout24\tutorial\Used cars for sale - AutoScout24.html"]
-    start_urls = ['https://www.autoscout24.com/lst?sort=standard&desc=0&ustate=N%2CU&atype=C']
+    start_urls = [#'https://www.autoscout24.com/lst?sort=standard&desc=0&ustate=N%2CU&atype=C']
+                   'file:///C:/Users/roblo/autoscout24/tutorial/locallyStoredWebPage.html'
+                ]
     #start_urls = ['https://www.autoscout24.com/lst/?sort=standard&desc=0&ustate=N%2CU&size=20&page=1&atype=C&']
     #start_urls = ["file:///C:/Users/roblo/autoscout24/tutorial/aap.htm"]
 
@@ -19,14 +21,13 @@ class ExampleSpider(scrapy.Spider):
             }
 
         #crawl to next page
-        page=idea.css('h3.card-title a::attr(href)').extract_first()
-        yield response.follow('https://ideas.lego.com'+page, self.parse)
+        #page=idea.css('h3.card-title a::attr(href)').extract_first()
+        #yield response.follow('https://ideas.lego.com'+page, self.parse)
 
         #example of xpath selector
-        response.xpath('//*/a[text()[contains(.,"Go to next page")]]/../../div[3]/p/text()').extract()[0]
+        response.xpath('//*/a[text()[contains(.,"Go to next page")]]/../../div[3]/p/text()').extract()
 
         #examples of copied selectors for nexpt page 
         #__next > div > div.css-xanrtl > div.css-160klfq.ey3mfx31 > main > div.css-1yuhvjn > nav > ul > li.css-jmbzf4 > a
-        //*[@id="__next"]/div/div[4]/div[2]/main/div[15]/nav/ul/li[14]/a
-        /html/body/div[1]/div[3]/div/div/div[4]/div[2]/main/div[15]/nav/ul/li[14]/a
-        
+        #//*[@id="__next"]/div/div[4]/div[2]/main/div[15]/nav/ul/li[14]/a
+        #/html/body/div[1]/div[3]/div/div/div[4]/div[2]/main/div[15]/nav/ul/li[14]/a
